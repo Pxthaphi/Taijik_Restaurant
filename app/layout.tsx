7dda917a17cc1486970e42d21a78f7738daf3cbb
pxthaphi <ohmzaza21@gmail.com>
2025-02-27 12:40:24 +0700

    Fix fonts all page to DB

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,26 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import Head from "next/head";
 import "./globals.css";
 import { Providers } from "./providers";
+import localFont from 'next/font/local'
 
+// const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({
+  src: [
+    {
+      path: './assets/fonts/DB_v4.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './assets/fonts/DB_Med_v4.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+})
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ร้านอาหารใต้จิก",
@@ -22,7 +37,7 @@ export default function RootLayout({
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body className={inter.className}>
+      <body className={myFont.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
